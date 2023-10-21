15f9921411f559872e0cd70e9e25b3e5de72e1ae
Cesar3600 <cesar.contreras.medina.3600@gmail.com>
2023-10-20 20:57:11 -0500

    feat(main.tsx App.tsx): remove default jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+
+import './styles/css/App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-
-      <p className="message">
-        Click on the Vite and React logos to learn more
-      </p>
+      iniciando!
     </>
   )
 }
