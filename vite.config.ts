166f8dea32f063da0b2df2a0fc72d90e0c71fcef
cesarContreras33 <cesar.contreras.front.challenge@gmail.com>
2023-10-23 07:34:24 -0500

    chore(config): udpate package.json, package-lock.json, tsconfig.json, vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
 import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test:{
+    globals: true,
+    environment: "jsdom",
+    css:true
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/styles/scss/variables.scss";`
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+      components: '/src/components',
+      styles: '/src/styles'
+    }
+  },
+  build:{
+    outDir:'carpetita'
+  }
 })
