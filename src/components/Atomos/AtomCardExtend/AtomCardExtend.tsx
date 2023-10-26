abd517ea2fee936580b7b234649e3d1ed4f9beb8
cesarContreras33 <cesar.contreras.front.challenge@gmail.com>
2023-10-26 09:47:33 -0500

    style(fix): error pm styles in atomCardExtend

--- src/components/Atomos/AtomCardExtend/AtomCardExtend.tsx
+++ src/components/Atomos/AtomCardExtend/AtomCardExtend.tsx
@@ -32,39 +32,41 @@ const AtomCardExtend: React.FC<AtomCardExtendProps> = ({
   return (
     <div className="atomcard-extend">
       <div className="atomcard-extend__content">
-        <div className="atomcard-extend__content--header">
-          <div className="atomcard-extend__content--header-pill">
-            <AtomicPill message={'plan recomendado'} />
-          </div>
-          <div className="atomcard-extend__content--header-title">
-            <div className="atomcard-extend__content--header-price">
-              <h2>{name}</h2>
-              <p className="plan">costo de plan</p>
-              <p className="antes">{discount && `${price} descuento`}</p>
-              <p className="cost">{`$${
-                discount ? `${price * 0.95}` : price
-              } al mes`}</p>
+        <div className="atomcard-extend__content--top">
+          <div className="atomcard-extend__content--header">
+            <div className="atomcard-extend__content--header-pill">
+              <AtomicPill message={'plan recomendado'} />
             </div>
-            <img src={house} alt="" />
+            <div className="atomcard-extend__content--header-title">
+              <div className="atomcard-extend__content--header-price">
+                <h2>{name}</h2>
+                <p className="plan">costo de plan</p>
+                <p className="antes">{discount && `${price} descuento`}</p>
+                <p className="cost">{`$${
+                  discount ? `${price * 0.95}` : price
+                } al mes`}</p>
+              </div>
+              <img src={house} alt="" />
+            </div>
           </div>
-        </div>
-        <div className="atomcard-extend__content--list">
-          <ul className="plan-description">
-            {description.map((descrip, index) => {
-              return (
-                <li key={index} className="plan-description__item">
-                  <div className="plan-description__item-box">
-                    <p>{descrip}</p>
-                  </div>
-                </li>
-              )
-            })}
-          </ul>
+          <div className="atomcard-extend__content--list">
+            <ul className="plan-description">
+              {description.map((descrip, index) => {
+                return (
+                  <li key={index} className="plan-description__item">
+                    <div className="plan-description__item-box">
+                      <p>{descrip}</p>
+                    </div>
+                  </li>
+                )
+              })}
+            </ul>
+          </div>
         </div>
         <div className="atomcard-extend__content--button">
           <AtomicButton
             label={'Seleccionar Plan'}
-            onClick={()=>handleSelectPlan(id)}
+            onClick={() => handleSelectPlan(id)}
           />
         </div>
       </div>
