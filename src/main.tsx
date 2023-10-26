72e7755daf4a89f18f1fa6491959ed4f96ed2136
cesarContreras33 <cesar.contreras.front.challenge@gmail.com>
2023-10-26 01:06:21 -0500

    fix: main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './styles/css/index.css'
+import { Provider } from 'react-redux'
+import {store} from './redux/store'
+import './styles/index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>
 )
