fb029fa018e957cb2e241fb80921cbf0771ba9a4
cesarContreras33 <cesar.contreras.front.challenge@gmail.com>
2023-10-26 04:42:05 -0500

    fix: on store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,14 +1,13 @@
 
 import {configureStore} from '@reduxjs/toolkit'
 import clientReducer from './slices/clientSlice'
-//import plansReducer from './slices/plansSlice'
-//import infoReducer from './slices/infoSlice'
+import plansReducer from './slices/plansSlice'
+
 
 export const store = configureStore({
   reducer: {
     client: clientReducer,
-/*     info:infoReducer,
-    plans: plansReducer, */
+    plans: plansReducer
   }
 })
 
