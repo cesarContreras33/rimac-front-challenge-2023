c3eae47ad71c55f59d9d931c5b30bf443c5615c8
cesarContreras33 <cesar.contreras.front.challenge@gmail.com>
2023-11-05 17:44:05 -0500

    feat(config): added alias

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@
 /// <reference types="vite/client" />
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -20,18 +21,20 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': './src',
-      '@components': './src/components',
-      '@assets': './src/assets',
-      '@styles': './src/styles',
-      '@fonts': './src/assets/fonts',
-      '@images':'./src/assets/images',
-      '@atomos':'./src/components/Atomos',
-      '@moleculas':'./src/components/Moleculas',
-      '@organismos':'./src/components/Organismos',
-      '@template':'./src/components/template',
-      '@pages':'./src/components/Pages',
-      '@constants':'./src/assets/constants'
+      "@": path.resolve(__dirname,"src"),
+      "@styles": path.resolve(__dirname,"src/styles"),
+      "@fonts": path.resolve(__dirname,"src/assets/fonts"),
+      "@types": path.resolve(__dirname,"src/types"),
+      "@images": path.resolve(__dirname,"src/assets/images"),
+      "@constants": path.resolve(__dirname,"src/assets/constants"),
+      "@atoms": path.resolve(__dirname,"src/components/Atomos"),
+      "@molecules": path.resolve(__dirname,"src/components/Moleculas"),
+      "@organism": path.resolve(__dirname,"src/components/Organismos"),
+      "@template": path.resolve(__dirname,"src/components/template"),
+      "@pages": path.resolve(__dirname,"src/components/Pages"),
+      "@redux": path.resolve(__dirname,"src/redux"),
+      "@utils": path.resolve(__dirname,"utils"),
+      "@api": path.resolve(__dirname,"src/api")
     }
   },
   build:{
