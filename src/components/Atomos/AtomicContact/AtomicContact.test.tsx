43d4cfec66d4c153a9b33a22191b4312d5da2399
cesarContreras33 <cesar.contreras.front.challenge@gmail.com>
2023-11-09 02:49:33 -0500

    test(feat): added atomicContact unit test

--- src/components/Atomos/AtomicContact/AtomicContact.test.tsx
+++ src/components/Atomos/AtomicContact/AtomicContact.test.tsx
@@ -1,10 +1,10 @@
 import { render } from "@testing-library/react";
-import { describe, test } from "vitest";
+import { describe, expect, test } from "vitest";
 import AtomicContact from "./AtomicContact";
 
 describe('AtomicContact unit test', () => {
   const msg= 'message'
-  const imgurl = 'http://okurl.com'
+  const imgurl = 'http://okurl.com/mamita.jpg'
   const tlf = '909828833'
   const component = render(<AtomicContact msg={msg} imgurl={imgurl} tlf={tlf}/>)
 
@@ -17,6 +17,10 @@ describe('AtomicContact unit test', () => {
   test('render component', () => {
     const {container} = component
     expect(container.innerHTML).toMatchSnapshot()
+  })
 
+  test('validating imgurl be url to image', () => {
+    const validurl = /^http:\/\/.*\.(jpg|png|jpeg)$/.test(imgurl)
+    expect(validurl).toBe(true)
   })
 })
