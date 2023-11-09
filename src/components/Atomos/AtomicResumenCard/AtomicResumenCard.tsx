aa35e667aba6ed9d2d23bfa41978b565e5aa0287
cesarContreras33 <cesar.contreras.front.challenge@gmail.com>
2023-11-09 17:32:50 -0500

    feat(update): added constants

--- src/components/Atomos/AtomicResumenCard/AtomicResumenCard.tsx
+++ src/components/Atomos/AtomicResumenCard/AtomicResumenCard.tsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import familia from '@images/ic_family.svg'
 import { AtomicResumenCardProps } from '@types/types'
+import { RESUMEN_TITLE, RESUMEN_SUBTITLE,RESUMEN_CELL,RESUMEN_PLAN,RESUMEN_COSTO_PLAN } from '@constants/constants'
 import './AtomicResumenCard.scss'
 
 
@@ -11,26 +12,26 @@ const AtomicResumenCard: React.FC<AtomicResumenCardProps> = ({ dataclient }) => 
     <div className="atomic-resumencard">
       <div className="atomic-resumencard__content">
         <div className="atomic-resumencard__content--user">
-          <span>Precios calculados para:</span>
+          <span>{RESUMEN_TITLE}</span>
           <div className="atomic-resumencard__content--user-data">
             <img src={familia} />
             <p>{`${dataclient.name} ${dataclient.lastName}`}</p>
           </div>
         </div>
         <div className="atomic-resumencard__content--payment">
-          <p>Responsable de pago</p>
+          <p>{RESUMEN_SUBTITLE}</p>
           <p>
             {dataclient.tipoDoc}: <span>{dataclient.nroDoc}</span>
           </p>
           <p>
-            Celular: <span>{dataclient.nroCell}</span>
+            {RESUMEN_CELL} <span>{dataclient.nroCell}</span>
           </p>
         </div>
         <div className="atomic-resumencard__content--plan">
-          <p>Plan elegido</p>
+          <p>{RESUMEN_PLAN}</p>
           <p>{dataclient.plan}</p>
           <p>
-            Costo del Plan: <span>{`$${dataclient.price}`}</span> al mes
+            {RESUMEN_COSTO_PLAN}<span>{`$${dataclient.price}`}</span> al mes
           </p>
         </div>
       </div>
